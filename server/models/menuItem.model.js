53557f4099dba2b2e23dd208fa1c30f080013f1d
Hafiz-IIT <syedhafizali4@gmail.com>
2025-06-02 04:41:54 +0530

    Update menuItem.model.js

--- server/models/menuItem.model.js
+++ server/models/menuItem.model.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-const menuItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  category: String,
-  price: { type: Number, required: true },
-  tags: [String],
+const orderSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  tableNumber: { type: Number, required: true },
+  items: [
+    {
+      name: String,
+      quantity: Number,
+    },
+  ],
+  status: { type: String, default: 'pending' },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('MenuItem', menuItemSchema);
+module.exports = mongoose.model('Order', orderSchema);
