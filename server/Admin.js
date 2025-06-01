fe520717cfb880371956e914c6df0bd6ad82301f
Hafiz-IIT <syedhafizali4@gmail.com>
2025-06-02 04:37:34 +0530

    Update and rename server/Admin.js to server/models/Admin.model.js

--- server/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  username: String,
-  password: String, // Hashed
-});
-
-module.exports = mongoose.model('Admin', adminSchema);
